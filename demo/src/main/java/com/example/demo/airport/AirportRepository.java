84354f52a424cc60bc95035fbd8ec86534e014f2
israel7970 <israel.alcocer.695@my.csun.edu>
2024-11-22 23:00:50 -0800

    Update comments AirportRepository.java

--- demo/src/main/java/com/example/demo/airport/AirportRepository.java
+++ demo/src/main/java/com/example/demo/airport/AirportRepository.java
@@ -1,11 +1,32 @@
 package com.example.demo.airport;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Interface name: Airport Repository
+ * @since 2024-11-15
+ * @author Miguel Santoscoy
+ * The {@code AirportRepository} interface provides the mechanism for CRUD operations 
+ * on {@link Airport} entities. It extends {@link JpaRepository} which provides built-in methods 
+ * for common database operations.
+ * <p>
+ * This repository is responsible for accessing the {@code Airport} data store and 
+ * interacting with the database. It allows for operations such as saving, finding, 
+ * and deleting airport records.
+ * </p>
+ * <p>
+ * The interface is marked with the {@link Repository} annotation to indicate that 
+ * it is a Spring Data repository and it will be automatically detected and managed 
+ * by Spring's dependency injection container.
+ * </p>
+ * 
+ * @author [Your Name]
+ * @since [Version]
+ */
 @Repository
-public interface AirportRepository extends JpaRepository<Airport, Integer>{
+public interface AirportRepository extends JpaRepository<Airport, Integer> {
 
+    // No additional methods are defined here as JpaRepository provides all the 
+    // necessary CRUD operations for interacting with the database.
 }
-
