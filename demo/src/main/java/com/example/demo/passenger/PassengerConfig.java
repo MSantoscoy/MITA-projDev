1c88d0ff9921116eb3a42216fbba387ae8a7fcbc
israel7970 <israel.alcocer.695@my.csun.edu>
2024-11-22 23:19:40 -0800

    Update PassengerConfig.java

--- demo/src/main/java/com/example/demo/passenger/PassengerConfig.java
+++ demo/src/main/java/com/example/demo/passenger/PassengerConfig.java
@@ -1,19 +1,19 @@
-//package com.example.demo.passenger;
-//
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class PassengerConfig {
-//
-//    @Bean
-//    CommandLineRunner commandLineRunner(PassengerRepository repository){
-//        return args-> {
-//
-//        };
-//        repository.saveAll(
-//
-//        );
-//    };
-//}
+package com.example.demo.passenger;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+/ublic class PassengerConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(PassengerRepository repository){
+        return args-> {
+
+        };
+        repository.saveAll(
+
+        );
+  };
+}
