f44f95626e3e9e2a10512d786a92881f208aaec4
israel7970 <israel.alcocer.695@my.csun.edu>
2024-11-25 16:36:34 -0800

    Payment repository interface

---
+++ demo/src/main/java/com/example/demo/payment/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.payment;
+
+import com.example.demo.payment.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+}
